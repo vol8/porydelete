78bf4f044edf372c4d8f7aa9108e020883afa80b
Voluptua <Drinemini@outlook.de>
2023-08-30 20:32:39 +0200

    Fixed result for '-a'. But if '<valid_attr> -a', result is buggy

--- src/ma.rs
+++ src/ma.rs
@@ -76,12 +76,14 @@ pub fn parse_file_and_delete_attribute(
     dir: &Path,
     args: &Vec<String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    // RESULT DATA
+
+// COLLECTING RESULT DATA
     let mut change_count = 0; // counter for successful changes
     let mut failed_count = 0; // counter for failed changes
     let mut failed_maps: Vec<PathBuf> = Vec::new(); // Array which holds map paths of all changes which failed for that map
     let mut selected_attributes: Vec<String> = vec![]; // Array which holds all attributes selected via arguments
-    let available_attributes = vec![ // Array which holds all available attributes
+    let available_attributes = vec![
+        // Array which holds all available attributes
         "connections",
         "object_events",
         "warp_events",
@@ -96,12 +98,19 @@ pub fn parse_file_and_delete_attribute(
             found_arg = true;
         } else if found_arg == false {
             eprintln!("Error: Unknown argument `{}`", args[i]);
+        }
+
+        // we only want this to be printed once when we have an wrong argument, even if we have multiple wrong arguments
+        if i == args.len() - 1 && found_arg == false {
             eprintln!("Use './porydelete --ma --help' for more information.");
         }
     }
-    if dir.is_dir() && args.len() >= 3 { // checks if there are at least 3 arguments, since thats needed to modify something
 
-    // ALGORITHM
+// MAP-ATTRIBUTE MODIFICATION EXECUTION
+    if dir.is_dir() && args.len() >= 3 {
+        // checks if there are at least 3 arguments, since thats needed to modify something
+
+        // ALGORITHM
         for entry in fs::read_dir(dir)? {
             // Subdir
             let entry = entry?;
@@ -120,32 +129,45 @@ pub fn parse_file_and_delete_attribute(
                     &mut failed_count,
                     &mut failed_maps,
                 )?;
-            } /* else&& selected_attributes[0] != String::from("None")let mut map = serde_json::from_str(&text).expect("Error: Failed to read file");
-                  remove_attributes(&mut map, args);
-                  overwrite_json_with_changes(&map, &path);
-              } */
+            }
         }
     }
-    // RESULT
-        
+
+// RESULT PRINTING
     println!("\nResult:");
     if selected_attributes.len() == 0 {
         selected_attributes.push(String::from("None"));
     }
-    println!("  Maps successfully modified: {}", change_count / selected_attributes.len() as i16); // The devision is there because every deletion per map, counts the counter so for 3 attributes, the final count is 3 times as high
-    println!("  Map-Attributes:");
-    for ma in selected_attributes.iter() {
-        println!("      {}", ma);
+    
+    if selected_attributes[0] != String::from("-a") {
+        println!(
+            "  Maps successfully modified: {}",
+            change_count / selected_attributes.len() as i16
+        ); // The devision is there because every deletion per map, counts the counter so for 3 attributes, the final count is 3 times as high
+        println!("  Map-Attributes:");
+        for ma in selected_attributes.iter() {
+            println!("      {}", ma);
+        }
+    } else if selected_attributes[0] == String::from("-a") {
+        println!("  Maps successfully modified: {}", change_count / 5);
+        println!("  Map-Attributes:");
+        println!("      connections");
+        println!("      object_events");
+        println!("      warp_events");
+        println!("      bg_events");
+        println!("      coord_events");
     }
 
-    if failed_maps.len() >= 1 && selected_attributes[0] != String::from("None") { // If there is any failure and theres at least one available argument, do that
+    if failed_maps.len() >= 1 && selected_attributes[0] != String::from("None") {
+        // If there is any failure and theres at least one available argument, do that
         println!("  Maps failed to read: {}, Path(s):", failed_count);
-        for maps in failed_maps { // Prints path of every map failed to read because 'map.json' is not available
+        for maps in failed_maps {
+            // Prints path of every map failed to read because 'map.json' is not available
             println!("      {}", maps.display().to_string());
         }
         println!("\nFilename is not 'map.json'! Try to rename and then proceed.");
     } else {
-        println!("  Maps failed to read: 0"); // Why 0 instead of 'failed_count'? Because it doesn't need to be printed because it would just confuse other while printing. Why? Because the paths aren't printed. 
+        println!("  Maps failed to read: 0"); // Why 0 instead of 'failed_count'? Because it doesn't need to be printed because it would just confuse other while printing. Why? Because the paths aren't printed.
     }
     Ok(())
 }
