d48f47e5d814d5f57170218de2a4fbb88894a717
Voluptua <Drinemini@outlook.de>
2023-09-04 22:33:15 +0200

    Step 1 - 4 done for --m, step 1 not tested

--- src/m.rs
+++ src/m.rs
@@ -11,14 +11,13 @@
 //   6   In './data/maps/map_groups.json' in map-group, delete <target>
 //-----------------------------------------------------------------------------------------------------------------------------------------------------
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::fs::{self, File};
 
 // There are different map name strings we need for different deletion processes (see from line 5).
 // With 'n_which' we specify how the function should process 'n_map' to get the string we need.
-fn get_path(n_map: &String, dirs_string: &Vec<String>, n_which: u8) -> Option<std::path::PathBuf> {
-    match n_which {
-        
+fn get_path(n_map: &String, dirs_string: &Vec<String>, n_for_which_step: u8) -> Option<std::path::PathBuf> {
+    match n_for_which_step {
         2 => {
             let map_as_path = Path::new(&n_map);
             let t_data_maps_dir = Path::new(&dirs_string[0]).join(map_as_path);
@@ -35,8 +34,63 @@ fn get_path(n_map: &String, dirs_string: &Vec<String>, n_which: u8) -> Option<st
     }
 }
 
-// Does step one of the map deletion process.
-fn remove_map_connection(dirs_string: &Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
+fn camel_to_snake_with_map_prefix(input: &String) -> String {
+    let mut result = String::new();
+    let mut prev_char: Option<char> = None;
+
+    for current_char in input.chars() {
+        if let Some(prev) = prev_char {
+            // Check if the current character is uppercase and the previous character is not an underscore
+            if current_char.is_uppercase() && prev != '_' {
+                result.push('_');
+            }
+        }
+
+        result.push(current_char.to_ascii_uppercase());
+        prev_char = Some(current_char);
+    }
+    result = String::from("MAP_") + &result;
+    result
+}
+
+fn delete_object(args: &Vec<String>, dir: &Path, n_map: String) -> Result<(), Box<dyn std::error::Error>> {
+    for entry in fs::read_dir(dir)? {
+        if dir.is_dir() {
+            let entry = entry?;
+            let path = entry.path();
+
+            if path.file_name().and_then(|n| n.to_str()) == Some("map.json") {
+                let contents = fs::read_to_string(&path)?;
+                let mut map: serde_json::Value = serde_json::from_str(&contents).expect("Error: failed to parse map.json");
+                
+                if let Some(array) = map.as_array_mut() {
+                    // Remove objects with "id": "test"
+                    array.retain(|item| {
+                        if let Some(obj) = item.as_object() {
+                            if let Some(id) = obj.get("map") {
+                                return id != n_map.as_str();
+                            }
+                        }
+                        true
+                    });
+                    let modified_json = serde_json::to_string_pretty(&map).expect("Error: failed to serialize map.json");
+                    fstream::write_text(path, modified_json, true).unwrap();
+                } else {
+                    // Handle the case where the JSON data is not an array
+                    eprintln!("Error: map.json is not an array.");
+                }
+            } else if path.file_name().and_then(|n| n.to_str()) == Some("scripts.inc") {
+                continue;
+            } else if path.file_name().and_then(|n| n.to_str()) == Some("connections.inc") {
+                continue;
+            } else if path.file_name().and_then(|n| n.to_str()) == Some("events.inc") {
+                continue;
+            } else if path.file_name().and_then(|n| n.to_str()) == Some("header.inc") {
+                continue;
+            }
+        }
+    }
+    println!("Success: Deleted all map connecions to {}!", args[2]);
     Ok(())
 }
 
@@ -45,9 +99,12 @@ pub fn parse_file_and_delete_map(
     args: &Vec<String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
 
-    // Step 1
-
-
+    // Step 1 (untested)
+    {
+       let t_data_maps_dir = Path::new(&dirs_string[0]);
+       let n_map = camel_to_snake_with_map_prefix(&args[2]);
+       delete_object(args, &t_data_maps_dir, n_map)?;
+    }   
     
     // Step 2
     {
@@ -59,7 +116,7 @@ pub fn parse_file_and_delete_map(
                 println!("Success: '{}' successfully deleted!", t_data_maps_dir.display());
             }
         } else if !t_data_maps_dir.exists() {
-            eprintln!("Error: '{}' doesn't exist! To delete a map as safely as possible, you need to provide '{}'!", t_data_maps_dir.display(), t_data_maps_dir.display());
+            eprintln!("Warning: '{}' doesn't exist! This however doesn't affect the map deletion process!", t_data_maps_dir.display());
         }
     }
 
@@ -73,7 +130,7 @@ pub fn parse_file_and_delete_map(
                 println!("Success: '{}' successfully deleted!", t_data_layouts_dir.display());
             }
         } else if !t_data_layouts_dir.exists() {
-            eprintln!("Error: '{}' doesn't exist! To delete a map as safely as possible, you need to provide '{}'!", t_data_layouts_dir.display(), t_data_layouts_dir.display());
+            eprintln!("Warning: '{}' doesn't exist! This however doesn't affect the map deletion process!", t_data_layouts_dir.display());
         }
     }
 
