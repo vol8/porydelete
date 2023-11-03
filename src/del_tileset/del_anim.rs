aeb7985b9dd81dd622bda4f4178d6d7da1a3172d
Voluptua <80710668+Voluptua@users.noreply.github.com>
2023-11-03 15:44:44 +0100

    Fix fn_tileset_anim regex

--- src/del_tileset/del_anim.rs
+++ src/del_tileset/del_anim.rs
@@ -26,7 +26,7 @@ fn fn_init_tileset_anim(ts_name: &str) {
         let header_new = header_contents.replace(header_match.unwrap().as_str(), ""); 
         match fs::write("./include/tileset_anims.h", header_new) {
             Ok(o) => println!("Anims. 1.2: Deleted '{}' in './include/tileset_anims.h'", fn_name),
-            Err(e) => eprintln!("Fatal Error: Failed to write to './include/tileset_anims.h'"),
+            Err(e) => eprintln!("Fatal Error: Anims. 1.2: Failed to write to './include/tileset_anims.h'"),
         }
     }
     if source_match.is_none() {
@@ -35,21 +35,50 @@ fn fn_init_tileset_anim(ts_name: &str) {
         let source_new = source_contents.replace(source_match.unwrap().as_str(), "");
         match fs::write("./src/tileset_anims.c", source_new) {
             Ok(o) => println!("Anims. 1.1: Deleted '{}' in './src/tileset_anims.c", fn_name),
-            Err(e) => eprintln!("Fatal Error: Failed to write to '/src/tileset_anims.c'"),
+            Err(e) => eprintln!("Fatal Error: Anims. 1.1: Failed to write to '/src/tileset_anims.c'"),
         }
     }
 }
 
 fn fn_tileset_anim(ts_name: &str) {
+    let fn_name = ts_name.replace("gTileset", "TilesetAnim");
 
+    let re_dec = Regex::new(format!(r"static\s*void\s*{}\s*\(\w+\);", fn_name).as_str()).unwrap();
+    
+    let re_def_suffix = r#"\s*\(\w+\s*\w+\)\n\{\n(\s*if\s*\(\w+\s*%\s*\w+\s*==\s*\w+\)\n\s*\w+\(\w+\s*\/\s*\w+\);)*\n\}"#;
+    let re_def_suffix = r"\{([^{}]*|(?R))*\}";
+    let re_def = Regex::new(format!(r"static\s*void\s*{}\(\w+\s*\w+\)\n{}", fn_name, re_def_suffix).as_str()).unwrap();
+
+    let contents = fs::read_to_string("./src/tileset_anims.c").unwrap();
+
+    let dec_match = re_dec.find(&contents);
+    let def_match = re_def.find(&contents);
+
+    if dec_match.is_none() {
+        eprintln!("dec failed");
+    }
+    if def_match.is_none() {
+        eprintln!("def failed");    
+    }
+
+
+    /*if dec_match.is_none() || def_match.is_none() {
+        eprintln!("Error: Couldn't find function declaration '{}' in './src/tileset_anims.c'", fn_name);
+    } else {
+        let contents_new = &contents.replace(dec_match.unwrap().as_str(), "").replace(def_match.unwrap().as_str(), "");
+        match fs::write("./src/tileset_anims.c", contents_new) {
+            Ok(o) => println!("Anims. 2: Deleted '{}' in './src/tileset_anims.c", fn_name),
+            Err(e) => eprintln!("Fatal Error: Anims. 2: Failed to write to '/src/tileset_anims.c'"),
+        }
+    }*/
 }
 
 fn fn_queue_anim_tiles(ts_name: &str) {
-
+    println!("Not ready yet...");
 }
 
 fn tileset_anims_frame(ts_name: &str) {
-
+    println!("Not ready yet...");
 }
 
 pub fn execute_del(ts_name: &str) -> PdError {
